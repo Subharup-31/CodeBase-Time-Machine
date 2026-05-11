f0cd9e20de1364f262331764fcc9d2b221b57d80
Subharup <subharupn@gmail.com>
2026-05-11 18:46:33 +0000

    docs: document parameter bounds and returned types

--- app/api/graph/route.ts
+++ app/api/graph/route.ts
@@ -28,4 +28,4 @@ export async function GET(req: Request) {
     }
 }
 
-                                                                                                                                                  
+                                                                                                                                                    

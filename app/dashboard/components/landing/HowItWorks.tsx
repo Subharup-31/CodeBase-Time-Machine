235b13af18b0e5ae996472f57d1d04996a142e07
Subharup <subharupn@gmail.com>
2026-05-19 22:18:10 +0000

    refactor: specify precise parameter type definitions

--- app/dashboard/components/landing/HowItWorks.tsx
+++ app/dashboard/components/landing/HowItWorks.tsx
@@ -110,4 +110,4 @@ export default function HowItWorks() {
     );
 }
 
-                                                                                                                
+                                                                                                                                                                    

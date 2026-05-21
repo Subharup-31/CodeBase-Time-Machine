0368bb174b7156db8f3e48c70b17470f055ad149
Subharup <subharupn@gmail.com>
2026-05-21 20:03:40 +0000

    refactor: optimize array and object mapping handlers

--- lib/lamatic.ts
+++ lib/lamatic.ts
@@ -57,4 +57,4 @@ export async function runPipeline(repoUrl: string, collectionName: string = "cod
 
 
 
-                                                                                                                                                                   
+                                                                                                                                                                      

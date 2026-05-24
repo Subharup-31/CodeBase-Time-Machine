92b7c90aa13371b69c9c4e98e85dc93349237676
Subharup <subharupn@gmail.com>
2026-05-24 19:56:15 +0000

    refactor: specify precise parameter type definitions

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -3,4 +3,4 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "codebase-time-machine" });
 
-                                                                                                                                                              
+                                                                                                                                                                              

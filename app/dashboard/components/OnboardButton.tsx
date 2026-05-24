f6801b7321966e75a51c1ec87d1518d685acd395
Subharup <subharupn@gmail.com>
2026-05-24 10:13:41 +0000

    refactor: simplify helper function conditional structures

--- app/dashboard/components/OnboardButton.tsx
+++ app/dashboard/components/OnboardButton.tsx
@@ -205,3 +205,5 @@ function formatMarkdown(text: string) {
 
     return html;
 }
+
+                                                                                                                                                                            

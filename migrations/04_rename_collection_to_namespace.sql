0bd4874d43dba6b5228b94f0d960a9faa0f12731
Subharup <subharupn@gmail.com>
2026-06-12 17:00:24 +0530

    chore: make migration 04 idempotent

--- migrations/04_rename_collection_to_namespace.sql
+++ migrations/04_rename_collection_to_namespace.sql
@@ -1,11 +1,22 @@
 -- Migration 04: Rename Collection to Namespace
 -- Aligns repositories table with Pinecone Namespace terminology
 
--- 1. Rename column collection to namespace in repositories table
-ALTER TABLE repositories RENAME COLUMN collection TO namespace;
+DO $$
+BEGIN
+    -- 1. Rename column collection to namespace in repositories table if it exists
+    IF EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name = 'repositories' 
+          AND column_name = 'collection'
+    ) THEN
+        ALTER TABLE repositories RENAME COLUMN collection TO namespace;
+    END IF;
+END $$;
 
 -- 2. Drop old collection unique constraint if exists
 ALTER TABLE repositories DROP CONSTRAINT IF EXISTS repositories_collection_key;
 
 -- 3. Add unique constraint on namespace column
+ALTER TABLE repositories DROP CONSTRAINT IF EXISTS repositories_namespace_key;
 ALTER TABLE repositories ADD CONSTRAINT repositories_namespace_key UNIQUE (namespace);
